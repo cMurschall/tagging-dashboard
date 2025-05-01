168563ff953d410d9b5cb86a30b5ca96dccbbe67
Christian Murschall <christian.murschall@coherent.com>
2025-05-01 13:39:39 +0200

    feat: add keyup event handling for BlurUpdateInput component

--- app.ui/src/components/BlurUpdateInput.vue
+++ app.ui/src/components/BlurUpdateInput.vue
@@ -1,31 +1,33 @@
 <template>
-    <BFormInput
-      v-model="localValue"
-      @blur="emitUpdate"
-      v-bind="$attrs"/>
-  </template>
+  <BFormInput v-model="localValue" @blur="emitUpdate" @keyup.enter="emitUpdate" @keyup.esc="resetLocalValue"
+    v-bind="$attrs" />
+</template>
 
-  <script setup lang="ts">
-  import { BFormInput } from "bootstrap-vue-next"
-  import { ref, watchEffect } from 'vue'
+<script setup lang="ts">
+import { BFormInput } from "bootstrap-vue-next"
+import { ref, watchEffect } from 'vue'
 
-  const props = defineProps<{
-    modelValue: string | null | undefined
-  }>()
+const props = defineProps<{
+  modelValue: string | null | undefined
+}>()
 
-  const emit = defineEmits<{
-    (e: 'update:modelValue', value: string): void
-  }>()
+const resetLocalValue = () => {
+  localValue.value = props.modelValue ?? ''
+}
 
-  const localValue = ref('')
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
 
-  // Keep localValue in sync with modelValue
-  watchEffect(() => {
-    localValue.value = props.modelValue ?? ''
-  })
+const localValue = ref('')
 
-  // Emit only on blur
-  const emitUpdate = () => {
-    emit('update:modelValue', localValue.value)
-  }
-  </script>
+// Keep localValue in sync with modelValue
+watchEffect(() => {
+  localValue.value = props.modelValue ?? ''
+})
+
+// Emit only on blur
+const emitUpdate = () => {
+  emit('update:modelValue', localValue.value)
+}
+</script>
